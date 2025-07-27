f4f10581f23e146ba3a951455233f48c78b09904
IJAHARKHAN <khan7oct@gmail.com>
2025-07-27 11:49:45 +0530

    Get Latest

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-let moon = 'hHello Moon'
+
+
+function createUser() {
+  let user = {
+    name: "Akshay"
+  };
+  // user object यहाँ memory में है
+  return user;
+}
+
+
+
+let person = createUser(); 
+
+console.log(person)
+console.log(person.name)
+
+
+let person2 = () => {
+  let user = {
+    name: "Akshay",
+    age: 25
+  };
+  return user;
+}
+
+let person1 = person2();
+console.log(person1.age);
+
+
+
+
+
+
