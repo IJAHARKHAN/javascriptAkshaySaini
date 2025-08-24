f85383edebf4ebf4c884e8db2b496bf2bca9ec85
IJAHARKHAN <khan7oct@gmail.com>
2025-08-24 13:08:15 +0530

    Get Latest

--- index.js
+++ index.js
@@ -1,34 +1,9 @@
-
-
-function createUser() {
-  let user = {
-    name: "Akshay"
-  };
-  // user object यहाँ memory में है
-  return user;
-}
-
-
-
-let person = createUser(); 
-
-console.log(person)
-console.log(person.name)
-
-
-let person2 = () => {
-  let user = {
-    name: "Akshay",
-    age: 25
-  };
-  return user;
-}
-
-let person1 = person2();
-console.log(person1.age);
-
-
-
-
-
-
+/*
+1. What is async ?
+2. What is await ?
+3. How aync await works behind the scenes ?
+5. Example of using async await.
+6. Error handling in async await.
+7. Difference between async await and promise.then/.catch.
+8. interview questions on async await.
+*/
