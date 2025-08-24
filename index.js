5c546e505fdc9285f615dd314632b87719b759b7
IJAHARKHAN <khan7oct@gmail.com>
2025-08-24 19:33:11 +0530

    Get Latest

--- index.js
+++ index.js
@@ -5,5 +5,26 @@
 5. Example of using async await.
 6. Error handling in async await.
 7. Difference between async await and promise.then/.catch.
-8. interview questions on async await.
+8. Interview questions on async await.
 */
+
+/// https://api.github.com/  main url of github api
+/// https://api.github.com/users  list of users
+
+let myGithubApi = "https://api.github.com/users/IJAHARKHAN";
+
+async function getUserData(){  
+  try {
+    let response = await fetch(myGithubApi);   
+    let data = await response.json();   
+    console.log(data);
+    
+  } catch (error) {
+    console.log("This is my error : " + error);
+    
+  }
+    
+}
+
+getUserData();
+
